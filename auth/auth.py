301e4f5437b9143133e6d39286f784b2ccae66fe
CarterCobb <carter.cobb72@gmail.com>
2022-01-12 17:02:04 -0700

    Added better testing

--- auth/auth.py
+++ auth/auth.py
@@ -1,13 +1,16 @@
 import os
 import boto3
-from jwt import decode, encode
-import jwt
+from jwt import decode, DecodeError
 
-JWT_SECRET = "12121212"  # os.environ["SECRET"]
+# JWT_SECRET = os.environ["SECRET"]
+JWT_SECRET = "rtINZYEEUWkHJ8gmCDyQyfqDZVAROUttk99e9MIpHDc97KbUeduDngegXMhj5BAG6dKlSmr9k5uGaiQh"
 dynamodb = boto3.resource("dynamodb")
 
 
 def lambda_handler(event, context):
+    """
+    Authorizes the user and returns applicable user authorization status.
+    """
     try:
         auth_token = event["headers"]["Authorization"]
         payload = decode(auth_token, JWT_SECRET, algorithms=["HS256"])
@@ -25,7 +28,7 @@ def lambda_handler(event, context):
             "error": "NO_AUTH_TOKEN",
             "message": "Failed to authentice"
         }
-    except jwt.exceptions.DecodeError:
+    except DecodeError:
         return {
             "statusCode": 500,
             "error": "DECODE_ERROR",
@@ -39,9 +42,8 @@ def lambda_handler(event, context):
         }
     return {"statusCode": 200, "user": user["Item"]}
 
-# TESTING 
-auth = encode({"id": "asdsad3q4324"}, JWT_SECRET, algorithm="HS256")
-print(auth)
 
-lambda_ = lambda_handler({"headers": {"Authorization": auth}}, None)
-print(lambda_)
+# TESTING
+data = {"headers": {"Authorization": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXNzd29yZCI6IiQyYiQxMiR4NkZRN3pRNEwxRTBhVXI0TncvOTNlUmI2UTdlcDQza2YxUUE3V1JPUEgzYXRpdmhUVHBTVyIsImVtYWlsIjoiY2FydGVyLmNvYmI3MkBnbWFpbC5jb20iLCJpZCI6ImFzZHNhZDNxNDMyNCJ9.N9kMZtnEkJXTIkTKVG8cfOx5csJT6YxgfpfnHn88EJs"}, "isBase64Encoded": False}
+
+print(lambda_handler(data, None))
